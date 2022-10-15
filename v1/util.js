9951302f3bafb5a7c50076498cb75799a4530d96
Max Leow <maxengiu@outlook.com>
2022-10-16 00:52:48 +0800

    add missing utf8 encoding function

--- v1/util.js
+++ v1/util.js
@@ -1,11 +1,36 @@
 {
     _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    _utf8_encode: function(string){
+        string = string.replace(/\r\n/g,"\n");
+        let utftext = "";
+    
+        for (let n = 0; n < string.length; n++) {
+    
+            let c = string.charCodeAt(n);
+    
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+    
+        }
+    
+        return utftext;
+    },
     encode: function(input) {
         let output = "";
         let chr1, chr2, chr3, enc1, enc2, enc3, enc4;
         let i = 0;
     
-        input = Base64._utf8_encode(input);
+        input = this._utf8_encode(input);
     
         while (i < input.length) {
     
