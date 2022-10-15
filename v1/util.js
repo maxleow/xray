fe7d7c8e6c504c6701a5a69d013dda59399bd47a
Max Leow <maxengiu@outlook.com>
2022-10-16 00:50:19 +0800

    change btoa to custom encoding

--- v1/util.js
+++ v1/util.js
@@ -1,4 +1,37 @@
 {
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    encode: function(input) {
+        let output = "";
+        let chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        let i = 0;
+    
+        input = Base64._utf8_encode(input);
+    
+        while (i < input.length) {
+    
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
+    
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
+    
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+    
+            output = output +
+            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+    
+        }
+    
+        return output;
+    },
     login: function(pm){
         let token_key = 'xray_token';
         let token = pm.globals.get(token_key);
@@ -32,7 +65,7 @@
 
         let test_run_key = pm.collectionVariables.get("xray_testrun_key");
 
-        let evidences = btoa(JSON.stringify({
+        let evidences = this.encode(JSON.stringify({
             requestHeader: pm.request.headers,
             requestBody: pm.request.body,
             responseHeader: pm.response.headers,
