d43465d0ce252956c734fba7d6b9a119174dc4c0
Max Leow <engiu.leow@digi.com.my>
2023-02-27 18:29:50 +0800

    update empty body

--- v5/util.js
+++ v5/util.js
@@ -100,27 +100,16 @@
 
         let test_run_key = pm.environment.get("xray_testrun_key") || pm.collectionVariables.get("xray_testrun_key");
 
-        try {
-            let evidences = this.encode(JSON.stringify({
-                requestUrl: pm.request.url.toString(),
-                requestParams: pm.request.url.getQueryString(),
-                requestHeader: pm.request.headers,
-                requestBody: (pm.request.method === "GET" || pm.request.code === 204)? {} : JSON.parse(pm.request.body.raw),
-                responseHeader: pm.response.headers,
-                responseBody: pm.response.json(),
-                responseCode: pm.response.code
-            }));
-        } catch (error) {
-            let evidences = this.encode(JSON.stringify({
-                requestUrl: pm.request.url.toString(),
-                requestParams: pm.request.url.getQueryString(),
-                requestHeader: pm.request.headers,
-                requestBody: {},
-                responseHeader: pm.response.headers,
-                responseBody: pm.response.json(),
-                responseCode: pm.response.code
-            }));
-        }
+        let evidences = this.encode(JSON.stringify({
+            requestUrl: pm.request.url.toString(),
+            requestParams: pm.request.url.getQueryString(),
+            requestHeader: pm.request.headers,
+            requestBody: (pm.request.method === "GET")? {} : JSON.parse(pm.request.body.raw),
+            responseHeader: pm.response.headers,
+            responseBody: (pm.request.code === 204)? {} : pm.response.json(),
+            responseCode: pm.response.code
+        }));
+        
         
 
         pm.sendRequest(
